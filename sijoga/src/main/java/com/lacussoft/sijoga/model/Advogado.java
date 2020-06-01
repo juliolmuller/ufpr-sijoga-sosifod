83f8074b2f93ffd7b04a70d3e0753be2daefec1f
Julio Muller <juliodolago@gmail.com>
2020-06-01 18:54:53 -0300

    Added attribute to Advogado entity

--- sijoga/src/main/java/com/lacussoft/sijoga/model/Advogado.java
+++ sijoga/src/main/java/com/lacussoft/sijoga/model/Advogado.java
@@ -1,9 +1,13 @@
 package com.lacussoft.sijoga.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class Advogado extends User {
+
+    @Column(name = "insc_oab", length = 10, unique = true)
+    private String oab;
 
     // TODO: remover construtores
     public Advogado() {}
@@ -16,5 +20,14 @@ public class Advogado extends User {
             "80610150", "Rua das Flores", 753,
             "Apartamento 65", "Curitiba", "PR"
         ));
+        oab = "12345";
+    }
+
+    public String getOab() {
+        return oab;
+    }
+
+    public void setOab(String oab) {
+        this.oab = oab;
     }
 }

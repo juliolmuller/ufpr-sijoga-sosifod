2f69384ab9ffc8154de4daede1d0ab5b6a17ffc5
Julio Muller <juliodolago@gmail.com>
2020-06-05 00:40:35 -0300

    Added CPF conversion to attribute setter

--- sijoga/src/main/java/com/lacussoft/sijoga/bean/LoginBean.java
+++ sijoga/src/main/java/com/lacussoft/sijoga/bean/LoginBean.java
@@ -1,6 +1,7 @@
 package com.lacussoft.sijoga.bean;
 
 import com.lacussoft.sijoga.security.AuthenticationBean;
+import com.lacussoft.utils.Converter;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -41,7 +42,7 @@ public class LoginBean implements Serializable {
     }
 
     public void setCpf(String cpf) {
-        this.cpf = cpf;
+        this.cpf = Converter.removeNonDigits(cpf);
     }
 
     public String getPassword() {

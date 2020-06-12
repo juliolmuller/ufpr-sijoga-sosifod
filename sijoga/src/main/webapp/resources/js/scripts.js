a65e2f197f9e8b9720dfe7f46d8c27c09354f1b9
Julio Muller <juliodolago@gmail.com>
2020-06-12 10:27:18 -0300

    Fixed 'OAB' field requirement and display

--- sijoga/src/main/webapp/resources/js/scripts.js
+++ sijoga/src/main/webapp/resources/js/scripts.js
@@ -29,6 +29,16 @@ $(function () {
         endDate: '-18y'
     });
 
+    $('[name="role"]').change(function() {
+        var role = $(this).val();
+        var oabField = $('#oabField');
+        if (role === 'ADVOGADO') {
+            $(oabField).show();
+        } else {
+            $(oabField).hide();
+        }
+    });
+
     var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIMqKwalYJtmvTwYi8ximHRZdOl2QBBlB5MIaWxwhXVBR5E_iiipj_z1xNVhiqjsljyK8XqL3Zj3ri/pub?gid=0&single=true&output=csv';
     var tipo = 'Advogado';
     var usuario = 'Josnei';

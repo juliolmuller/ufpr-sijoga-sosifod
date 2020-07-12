f5c6854ae1467dc3f9320c546bd9cf57687f39c6
Julio Muller <juliodolago@gmail.com>
2020-07-12 15:57:10 -0300

    Created methods to return key information

--- sijoga/src/main/java/com/lacussoft/sijoga/services/DaoFacade.java
+++ sijoga/src/main/java/com/lacussoft/sijoga/services/DaoFacade.java
@@ -1,15 +1,20 @@
 package com.lacussoft.sijoga.services;
 
+import com.lacussoft.sijoga.model.Juiz;
 import com.lacussoft.sijoga.model.Model;
+import com.lacussoft.sijoga.model.Process;
 import com.lacussoft.sijoga.model.User;
 import com.lacussoft.utils.HibernateUtil;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @Stateless
 public class DaoFacade implements Serializable {
@@ -43,5 +48,32 @@ public class DaoFacade implements Serializable {
 
     public <T extends Model> Criteria createCriteria(Class<T> clazz) {
         return session.createCriteria(clazz);
+    }
+
+    public List<Process> getProcessesFor(User user) {
+        Criteria criteria = createCriteria(Process.class);
+        criteria.createAlias("promoter.lawyer", "promoterLawyer");
+        criteria.createAlias("promoted.lawyer", "promotedLawyer");
+        criteria.add(Restrictions.or(
+            Restrictions.eq("judge.id", user.getId()),
+            Restrictions.eq("promoter.id", user.getId()),
+            Restrictions.eq("promoted.id", user.getId()),
+            Restrictions.eq("promoterLawyer.id", user.getId()),
+            Restrictions.eq("promotedLawyer.id", user.getId())
+        ));
+
+        return criteria.list();
+    }
+
+    public User findUserByCpf(String cpf)  {
+        return (User) createQuery("FROM User u WHERE u.cpf = :cpf")
+            .setString("cpf", cpf)
+            .uniqueResult();
+    }
+
+    public Juiz getRandomJudge() {
+        Random rand = new Random();
+        List<Juiz> judges = createQuery("FROM Juiz").list();
+        return judges.get(rand.nextInt(judges.size()));
     }
 }
